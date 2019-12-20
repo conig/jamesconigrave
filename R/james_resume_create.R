ef93d3447e0c9944ec80355673077a1c91de7a00
James Conigrave <james.conigrave@gmail.com>
2019-12-20 13:05:23 +1100

    update

--- R/james_resume_create.R
+++ R/james_resume_create.R
@@ -1,8 +1,18 @@
 #xaringan::inf_mr("james resume.rmd")
 
+#' resume
+#'
+#' open resume
+#' @export resume
+
 resume = function(){
 shell.exec(system.file("to_github/jamesconigrave_resume.pdf", package = "jamesconigrave"))
 }
+
+#' update_resume
+#'
+#' update resume on github
+#' @export update_resume
 
 update_resume = function(){
   md = system.file("to_github/jamesconigrave_resume.rmd", package = "jamesconigrave")
@@ -12,7 +22,8 @@ update_resume = function(){
   rmarkdown::render(md, output_file = system.file("to_github/docs/index.html", package = "jamesconigrave"))
   pagedown::chrome_print(md)
 
-  if(dir.exists(paste0(gh,"/.git"))){
+  if(!dir.exists(paste0(gh,"/.git"))){
+    message("git dir doesn't exist, initialising... ",glue::glue("{gh}"))
 
     shell(paste(glue::glue("cd {gh}"),
           "git init",
