cb79d5bdad09cce9eb1c231cce40fcfc850e0fc5
conig <james.conigrave@gmail.com>
2021-04-01 13:00:51 +1100

    rename package

--- R/papers.R
+++ R/papers.R
@@ -7,6 +7,7 @@ papers = function(){
   dir <- system.file("papers",  package= "jamesconigrave")
   papers <- list.files(dir)
 
+
   path <- select.list(
     title = "What paper would you like to open?",
     choices = papers
