b3d5c7b94129efe4f889bb40e0fdb9416ee802f1
James Conigrave <james.conigrave@gmail.com>
2024-07-01 17:35:57 +1000

    remove old dev container

--- .devcontainer/install.sh
+++
@@ -1,9 +0,0 @@
-export DEBIAN_FRONTEND=noninteractive
-
-echo "Updating package lists..."
-sudo apt-get update
-
-echo "Installing packages..."
-sudo apt-get install -y libxt6 libzmq3-dev xclip
-
-echo "Installation complete."
